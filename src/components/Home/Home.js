7e1fda8c2585db3cc27791deeb64b085044327fd
srishtirathi <srishti.cs17@bmsce.ac.in>
2021-03-15 11:17:27 +0530

    feat: add Home component with styles

---
+++ src/components/Home/Home.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Home.css';
+
+const Home = () => (
+  <div className="home">
+    <h4>Seems a bit empty in here :(</h4>
+    <button type="button">Sync</button>
+  </div>
+);
+export default Home;
